dbf8d2f7ec6f47cc67284f646808cab26bbd9678
Makio64 <davronai@gmail.com>
2024-02-02 00:51:12 +0100

    add visual feedback in renderer

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,12 +1,14 @@
 import { app, shell, BrowserWindow } from 'electron'
-import * as path from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import creatWorker from './worker?nodeWorker'
 import callFork from './fork'
+import path from 'path'
+
+let mainWindow
 
 function createWindow(): void {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
     show: false,
@@ -17,6 +19,8 @@ function createWindow(): void {
         }
       : {}),
     webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
       preload: path.join(__dirname, '../preload/index.js'),
       sandbox: false
     }
@@ -24,6 +28,9 @@ function createWindow(): void {
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
+    if (is.dev) {
+      mainWindow.webContents.openDevTools()
+    }
   })
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
@@ -31,8 +38,6 @@ function createWindow(): void {
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
@@ -40,45 +45,34 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
 
   createWindow()
 
-  creatWorker({ workerData: 'worker' })
-    .on('message', (message) => {
-      console.log(`\nMessage from worker: ${message}`)
-    })
-    .postMessage('')
+  setTimeout(() => {
+    creatWorker({ workerData: 'worker' })
+      .on('message', (message) => {
+        // Send the message directly to the renderer process
+        if (mainWindow) {
+          mainWindow.webContents.send('test', message)
+        }
+      })
+      .postMessage('')
 
-  callFork()
+    callFork()
+  }, 1000)
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,8 +1,15 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
 // Custom APIs for renderer
-const api = {}
+const api = {
+  receive: (channel, func) => {
+    ipcRenderer.on(channel, (event, ...args) => func(...args))
+  },
+  send: (channel, data) => {
+    ipcRenderer.send(channel, data)
+  }
+}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
